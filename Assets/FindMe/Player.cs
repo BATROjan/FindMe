a683e4e72c998751bfe95d2264a2b0fe5fd4364a
Batyzhan <>
2023-11-08 16:21:12 +0700

    save

--- Assets/FindMe/Player.cs
+++ Assets/FindMe/Player.cs
@@ -1,13 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
+    private Animator anim;
+    private Rigidbody _rigidbody;
+    public AnimationClip _animation;
+
+    private void Start()
+    {
+         anim = GetComponentInChildren<Animator>();
+         _rigidbody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
+        if (_rigidbody.velocity.magnitude>0)
+        {
+           anim.SetBool("Run",true); 
+        }
+        else
+        {
+            anim.SetBool("Run",false); 
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0, 0.5f, 0);
+        }
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
